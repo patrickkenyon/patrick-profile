037eb899a8def47702bcc1c6ac47e22123b16e42
Patrick <patrick.kenyon@gmail.com>
2018-10-01 13:10:29 +0100

    created php page

--- about_me.php
+++ about_me.php
@@ -6,14 +6,6 @@ $db->SetAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
 $stmt = $db->query("SELECT `bio_title`,`bio`,`contact_title`,`email`,`telephone` FROM `about_me`;");
 $about_me_data = $stmt->fetchAll();
 
-function placeholderData($newdata,$olddata,$submit) {
-    if (isset($submit)) {
-        echo $newdata;
-    } else {
-        echo $olddata;
-    }
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -25,7 +17,7 @@ function placeholderData($newdata,$olddata,$submit) {
 <body>
 
     <form method="post" action="about_me.php">
-        <input type="text" name="bio_title" value="<?php echo placeholderData($_POST["bio_title"], $about_me_data[0]["bio_title"], $_POST["submit"]) ?>"> Bio title<br/>
+        <input type="text" name="bio_title" value="<?php echo $about_me_data[0]["bio_title"] ?>"> Bio title<br/>
         <textarea rows="15" cols="50" name="bio"><?php echo $about_me_data[0]["bio"] ?></textarea> Bio<br/>
         <input type="text" name="contact_title" value="<?php echo $about_me_data[0]["contact_title"] ?>"> Contact title<br/>
         <input type="email" name="email" value="<?php echo $about_me_data[0]["email"] ?>"> Email<br/>

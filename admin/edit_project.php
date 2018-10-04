f224434399a6af852f0dc13bebc4d2e04b1b4ec0
Patrick <patrick.kenyon@gmail.com>
2018-10-04 11:54:17 +0100

    added delete to edit projects and changed fetchall to fetch

--- admin/edit_project.php
+++ admin/edit_project.php
@@ -7,7 +7,7 @@ $db = connectDatabase();
 $stmt = $db->prepare("SELECT `id`,`title`,`mini_description`,`background_image`,`project_url` FROM `projects` WHERE `id` = :id ;");
 $stmt->bindParam(':id', $_GET["id"]);
 $stmt->execute();
-$projects_data = $stmt->fetchAll();
+$projects_data = $stmt->fetch();
 
 ?>
 
@@ -22,13 +22,13 @@ $projects_data = $stmt->fetchAll();
 
     <form method="POST" action="submit_edit_project.php?id=<?php echo $_GET['id']; ?>" enctype="multipart/form-data">
         <label>Title</label>
-        <input type="text" name="title" value="<?php echo $projects_data[0]["title"]; ?>"><br/>
+        <input type="text" name="title" value="<?php echo $projects_data["title"]; ?>"><br/>
         <label>Mini description</label>
-        <textarea rows="15" cols="50" name="mini_description"><?php echo $projects_data[0]["mini_description"]; ?></textarea><br/>
+        <textarea rows="15" cols="50" name="mini_description"><?php echo $projects_data["mini_description"]; ?></textarea><br/>
         <label>Background image</label>
-        <input type="file" name="background_image" value="<?php echo $projects_data[0]["background_image"]; ?>"><br/>
+        <input type="file" name="background_image" value="<?php echo $projects_data["background_image"]; ?>"><br/>
         <label>Project url</label>
-        <input type="text" name="project_url" value="<?php echo $projects_data[0]["project_url"]; ?>"><br/><br/>
+        <input type="text" name="project_url" value="<?php echo $projects_data["project_url"]; ?>"><br/><br/>
         <input type="submit" value="Edit existing projects">
         <?php echo backButton('choose_project.php'); ?>
     </form>
